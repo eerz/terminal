9bf0c0fb6c6da66603e6a2f7141e7e5022a588c6
six <104098327+eerz@users.noreply.github.com>
2022-08-06 18:31:32 +0530

    whoami
    whoami update

--- js/commands.js
+++ js/commands.js
@@ -8,20 +8,12 @@ var email = 'mailto:forrest@fkcodes.com';
 
 whois = [
   "<br>",
-  "Hey, I'm Forrest!👋",
-  "I'm a software developer and content creator, who builds engaging websites like this one",
-  "and makes YouTube videos about computer science & software engineering.",
-  "After graduating with a Bachelor's in Computer Science, I worked professionally",
-  "as a software engineer building enterprise web applications for Fortune 500 companies.",
-  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
-  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
-  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
-  "I needed to make the jump from software engineering to full time content creator, and it's",
-  "the best decision I ever made.",
-  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
-  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
-  "because they're reading this right now, is that I also run a creative & media agency.",
-  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "Hey, I'm six!👋",
+  "I'm a block game player and a programmer 😎",
+  "I also make YouTube videos for minecraft hive / zeqa",
+  "I have not graduated yet",
+  "I'm still in my last year of high school",
+  "This is one of my first ever projects",
   "<br>"
 ];
 
@@ -55,12 +47,12 @@ projects = [
 
 help = [
   "<br>",
-  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whois</span>          Who is six?',
   '<span class="command">whoami</span>         Who are you?',
   '<span class="command">video</span>          View YouTube videos',
   '<span class="command">social</span>         Display social networks',
   '<span class="command">secret</span>         Find the password',
-  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">projects</span>       View my coding projects',
   '<span class="command">history</span>        View command history',
   '<span class="command">help</span>           You obviously already know what this does',
   '<span class="command">email</span>          Do not email me',
